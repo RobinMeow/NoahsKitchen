3ff58c3d6ffc05a33876ca9d8c4c43e5b0481276
robinmeow97@gmail.com <111490292+RobinMeow@users.noreply.github.com>
2023-11-01 13:49:38 +0100

    fix button adn theme

--- src/app/core/menu/menu.component.ts
+++ src/app/core/menu/menu.component.ts
@@ -2,11 +2,12 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatDrawer } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'core-menu',
   standalone: true,
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, MatIconModule, MatButtonModule],
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
