1c48421aa48c8cf386160bfe1f15122b373bb946
Robin <111490292+RobinMeow@users.noreply.github.com>
2023-11-17 23:03:37 +0100

    26 angular v17 (#27)
    * npm update
    
    * ng update schematics
    
    * ng update material
    
    * make use of shared module
    
    * remove unused imports

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,11 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { TranslateModule } from '@ngx-translate/core';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @Component({
   selector: 'core-home',
   standalone: true,
-  imports: [CommonModule, TranslateModule],
+  imports: [SharedModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
