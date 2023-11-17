0c92b66ab4956d15c68c6a9b82c6de0e11029b42
Robin <111490292+RobinMeow@users.noreply.github.com>
2023-11-18 00:55:35 +0100

    move to new build system (#33)

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { CORE_ROUTES } from './app/core/core.routes';
 
