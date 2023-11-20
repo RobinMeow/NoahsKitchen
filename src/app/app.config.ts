3340579e52b1e71b0ba0b309ab901e15bfc6506d
robinmeow97@gmail.com <111490292+RobinMeow@users.noreply.github.com>
2023-11-21 00:06:35 +0100

    sort imports in app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,11 @@ import { importProvidersFrom } from '@angular/core';
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+
 import { CORE_ROUTES } from './core/core.routes';
 import { provideOpenApiBasePath } from './providers/provideOpenApiBasePath';
 import { authInterceptor } from './core/auth/auth.interceptor';
-import { BrowserModule } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
