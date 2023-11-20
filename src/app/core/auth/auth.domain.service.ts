65fad6180125b01d7d7022a4c08c8ef92f325b50
robinmeow97@gmail.com <111490292+RobinMeow@users.noreply.github.com>
2023-11-20 22:56:34 +0100

    use signals and store token

--- src/app/core/auth/auth.domain.service.ts
+++ src/app/core/auth/auth.domain.service.ts
@@ -1,14 +1,31 @@
-import { Injectable, inject } from '@angular/core';
+import {
+  Injectable,
+  Signal,
+  WritableSignal,
+  inject,
+  signal,
+} from '@angular/core';
 import { firstValueFrom } from 'rxjs';
 import { AuthService, RegisterChefDto } from 'src/app/openapi-services';
 import { RegisterChef } from './register/RegisterChef';
 import { LoginCredentials } from './LoginCredentials';
+import { TokenStorage } from './token.storage';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthDomainService {
   private readonly _authService = inject(AuthService);
+  private readonly _tokenStorage = inject(TokenStorage);
+  private readonly _tokenSignal: WritableSignal<string | null | undefined> =
+    signal(undefined);
+
+  private readonly _key: string = 'token';
+
+  constructor() {
+    const token: string | null = this._tokenStorage.retrieve();
+    this._tokenSignal.set(token);
+  }
 
   registerAsync(chef: RegisterChef): Promise<void> {
     const dto: RegisterChefDto = {
@@ -17,12 +34,27 @@ export class AuthDomainService {
     return firstValueFrom(this._authService.authRegisterPost(dto));
   }
 
-  loginAsync(credentials: LoginCredentials): Promise<string> {
+  async loginAsync(credentials: LoginCredentials): Promise<string> {
     if (credentials.name.length === 0)
       throw new Error('Login name may not be missing.');
+
     if (credentials.password.length === 0)
       throw new Error('Login password may not be empty.');
 
-    return firstValueFrom(this._authService.authLoginPost(credentials));
+    const token = await firstValueFrom(
+      this._authService.authLoginPost(credentials),
+    );
+
+    this._tokenSignal.set(token);
+
+    return token;
+  }
+
+  /**
+   * @returns the token string when the chef is logged in or null if not.
+   * If it is undefined the tokenStorage has not been retrieved yet.
+   */
+  public getTokenSignal(): Signal<string | null | undefined> {
+    return this._tokenSignal.asReadonly();
   }
 }
--- src/app/core/menu/auth-corner/auth-corner.component.ts
+++ src/app/core/menu/auth-corner/auth-corner.component.ts
@@ -1,10 +1,15 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  Signal,
+  inject,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Subscription } from 'rxjs';
 import { MatDrawer } from '@angular/material/sidenav';
 import { Router, RouterLink } from '@angular/router';
-import { AuthService } from 'src/app/openapi-services';
 import { MatButtonModule } from '@angular/material/button';
+import { AuthDomainService } from '../../auth/auth.domain.service';
 
 @Component({
   selector: 'app-auth-corner',
@@ -15,22 +20,13 @@ import { MatButtonModule } from '@angular/material/button';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AuthCornerComponent {
-  private _sub: Subscription = Subscription.EMPTY;
-
-  protected isAuthenticated: boolean = false;
   @Input() drawer!: MatDrawer;
 
-  constructor(protected _authService: AuthService, private _router: Router) {
-    // this._sub = this._authService..isAuthenticated$.subscribe((isAuthed) => {
-    //   this.isAuthenticated = isAuthed;
-    // });
-  }
+  private readonly _authService = inject(AuthDomainService);
+  private readonly _router = inject(Router);
 
-  async logout() {
-    // await this._authService.logout();
-  }
+  protected tokenSignal: Signal<string | null | undefined> =
+    this._authService.getTokenSignal();
 
-  ngOnDestroy(): void {
-    this._sub.unsubscribe();
-  }
+  protected logout(): void {}
 }
