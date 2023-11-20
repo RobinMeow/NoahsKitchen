e68f2990cff0fe2cc6a603c8ef99ffbfac40aab9
robinmeow97@gmail.com <111490292+RobinMeow@users.noreply.github.com>
2023-11-21 00:04:27 +0100

    update animation import

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { ApplicationConfig } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { CORE_ROUTES } from './core/core.routes';
 import { provideOpenApiBasePath } from './providers/provideOpenApiBasePath';
@@ -12,7 +12,8 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideOpenApiBasePath(),
     provideHttpClient(withInterceptors([authInterceptor])),
-    importProvidersFrom(BrowserModule, BrowserAnimationsModule),
+    provideAnimations(),
+    importProvidersFrom(BrowserModule),
     provideRouter(CORE_ROUTES),
   ],
 };
