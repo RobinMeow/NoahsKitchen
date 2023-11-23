d0127cfc9fdc48f5553c6248f95d340f1ec11d1e
Robin <111490292+RobinMeow@users.noreply.github.com>
2023-11-23 23:53:22 +0100

    app title strategy (#40)

--- src/app/providers/provideOpenApiBasePath.ts
+++ src/app/providers/provideOpenApiBasePath.ts
@@ -1,8 +1,8 @@
-import { Provider } from '@angular/core';
+import { ValueProvider } from '@angular/core';
 import { BASE_PATH } from '../openapi-services';
 import { environment } from 'src/environments/environment';
 
-export const provideOpenApiBasePath = function (): Provider {
+export const provideOpenApiBasePath = function (): ValueProvider {
   return {
     provide: BASE_PATH,
     useValue: environment.API_BASE_URL,
